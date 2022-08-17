07b918aa0fb9de0718384f662f62921b134f7cd3
thiagowgarcia <thiagowgarcia@gmail.com>
2022-08-17 10:30:37 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let totalSliders = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+
+document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
+
+document.querySelector('.slider--controls').style.heigth = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+  (currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  }
+}
+updateMargin();
+    
+}
+function goNext() {
+  currentSlide++;
+  if (currentSlide > (totalSlides - 1)) {
+    currentSlide = 0;
+  }
+  updateMargin();
+
+}
+
+function updateMargin() {
+  let newMargin = (currentSlide * document.body.clientWidth);
+  document.querySelector('.slider--width ').style.marginLeft = `-${newMargin}px`;
+}
